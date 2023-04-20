95c5cefd1b3c3dfee63db12d81c7ddd41ec067ed
chojangg <s2115@e-mirim.hs.kr>
2023-04-20 14:07:55 +0900

    Console.SetCuorsorPosition 메서드 연습

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp
@@ -10,6 +11,27 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            int x = 1;
+            while (x < 50)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(x, 5);
+
+                if (x % 3 == 0)
+                {
+                    Console.WriteLine("__@");
+                }
+                else if (x % 3 == 1)
+                {
+                    Console.WriteLine("_^@");
+                }
+                else
+                {
+                    Console.WriteLine("^_@");
+                }
+                Thread.Sleep(1000);
+                x++;
+            }
         }
     }
 }
